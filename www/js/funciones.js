0b094d113612ae9be5e5bf80a1318c7347878d55
dierivera <drivera_22@hotmail.com>
2015-08-28 09:06:02 -0600

    Edit is now loading article content

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -2,6 +2,9 @@ var parseKey = "fxMmEKDgj8YsU6ViFsnynNpcTiXYqNlRKQHai5Oy";
 var appId = "m02AeiYtBKS6uwWorv5yfobZ2pC4r8I83ZUAnfie";
 var importantLimit = 10;
 var objectIds = [];
+var noticiaIds = [];
+var eventoIds = [];
+var proyectoIds = [];
 
 //Carga los articulos con prioridad y los articulos mas vistos
 function loadPublications() {  //main screen loading
@@ -104,7 +107,12 @@ function loadAdminArticles(type, contentDiv) {
 			// Do something with the returned Parse.Object values
 			for (var i = 0; i < results.length; i++) { 
 				var object = results[i];
-				objectIds[i] = object.id;
+				if (type == 1)
+					noticiaIds[i] = object.id;
+				if (type == 2)
+					eventoIds[i] = object.id;
+				if (type == 3)
+					proyectoIds[i] = object.id;
 				//document.getElementById(contentDiv).innerHTML = document.getElementById(contentDiv).innerHTML + '<div class="card"><div class="item item-divider">'+ object.get('title') + '</div></div>';
 				var favorito;
 				if (object.get('priority')){
@@ -114,8 +122,10 @@ function loadAdminArticles(type, contentDiv) {
 					favorito = '<a class="tab-item" href="#"><i class="icon ion-android-star-outline"></i>Favorito</a>';
 				}
 				var difundir = '<a class="tab-item" href="#"><i class="icon ion-android-notifications"></i> Difundir </a>';
-				var editar = '<a class="tab-item" href="#"><i class="icon ion-edit"></i> Editar</a>';
-				var eliminar = '<a class="tab-item" onclick="return deleteObject('+ i +');"><i class="icon ion-android-delete" ></i> Eliminar</a>';
+				var site = 'toEditPage('+ i +","+ type +');';
+				console.log(site);
+				var editar = '<a class="tab-item" onclick="return '+ site +'"><i class="icon ion-edit"></i> Editar</a>';
+				var eliminar = '<a class="tab-item" onclick="return deleteObject('+type + "," + i +');"><i class="icon ion-android-delete" ></i> Eliminar</a>';
 				//console.log('return deleteObject('+ i +')');
 				document.getElementById(contentDiv).innerHTML = document.getElementById(contentDiv).innerHTML + '<div class="card"> <div class="item item-text-wrap">' + object.get('title') + '<div class="item tabs tabs-secondary tabs-icon-left">' + favorito + difundir + editar + eliminar + '</div></div></div>';                                                                                                                                                                                                                                             
 				}
@@ -133,6 +143,21 @@ function loadAdminArticles(type, contentDiv) {
 function toPage(site) {
 		//location.href='login.html'; //ejemplo
 		location.href=site;
+		//location.href="createEvento.html?objectId=0";
+		//console.log("TEST");
+}
+
+function toEditPage(i,type){
+	var objectId;
+	if (type == 1){
+		objectId = noticiaIds[i];
+		location.href='editNoticia.html?objectId=' + objectId;}
+	if (type == 2){
+		objectId = eventoIds[i];
+		location.href='editEvento.html?objectId=' + objectId;}
+	if (type == 3){
+		objectId = proyectoIds[i];
+		location.href='editProyecto.html?objectId=' + objectId;}
 }
 	
 	
@@ -152,9 +177,15 @@ function createArticle(type){ //type can be: 'noticia', 'evento' or 'proyecto'
 	location.href="create" + type + ".html";
 }
 
-function deleteObject(i){
-	console.log(objectIds[i]);
-	objectId = objectIds[i];
+function deleteObject(type, i){
+	//console.log(objectIds[i]);
+	var objectId;
+	if (type == 1)
+		objectId = noticiaIds[i];
+	if (type == 2)
+		objectId = eventoIds[i];
+	if (type == 3)
+		objectId = proyectoIds[i];
 	Parse.initialize(appId, parseKey);
 	var Evento = Parse.Object.extend("Evento");		
 	var query = new Parse.Query(Evento);
@@ -162,25 +193,60 @@ function deleteObject(i){
 	query.find({
 		  success: function(results) {
 			var object = results[0];
-			console.log(object.get('title'));
-			object.destroy({
-			  success: function(object) {
-				alert("El elemento fue eliminado");
-				toPage("admin_index.html");
-			  },
-			  error: function(object, error) {
-				alert("Error al eliminar");
-			  }
-			});
+			var confirmation = confirm("Seguro que quiere eliminar el articulo?"); //popup con dos opciones
+			if (confirmation){ //si se le da que si, entra a ejecutar el eliminar
+				object.destroy({
+				  success: function(object) {
+					alert("El elemento fue eliminado");
+					toPage("admin_index.html");
+				  },
+				  error: function(object, error) {
+					alert("Error al eliminar");
+				  }
+				});
+			}else {
+				//Do Nothing
+			}
 		},
 	  error: function(error) {
-		alert("Error: " + error.code + " " + error.message);
+		//alert("Error: " + error.code + " " + error.message);
 		if (error.code = -1)
 			alert("Error de conexi�n, verifica la conexion de internet");
 	  }
 	});	
 }
 
+
+//Funcion llamada para enviar a la pantalla de crearX.
+function createArticle(type){ //type can be: 'noticia', 'evento' or 'proyecto'
+	//alert ("Going to: create" + type + ".html");
+	location.href="create" + type + ".html";
+}
+
+function editObject(){
+	var i = getUrlParameter('objectId');
+	Parse.initialize(appId, parseKey);
+	var Evento = Parse.Object.extend("Evento");		
+	var query = new Parse.Query(Evento);
+	query.equalTo("objectId", i);
+	query.find({
+		  success: function(results) {
+			var object = results[0];
+			$('#title').val(object.get('title'));
+			$('#brief_description').val(object.get('brief_description'));
+			$('#description').val(object.get('description'));
+			$('#professor').val(object.get('professor'));
+			$('#telephone').val(object.get('telephone'));
+			$('#email').val(object.get('email'));
+			$('#link').val(object.get('link'));
+		},
+	  error: function(error) {
+		//alert("Error: " + error.code + " " + error.message);
+		//if (error.code = -1)
+			//alert("Error de conexi�n, verifica la conexion de internet");
+	  }
+	});	
+}
 
 function login() {
 	username = document.getElementById("username").value;
@@ -210,5 +276,36 @@ function logout() {
 	}
 	else{
 		alert ("Error al intentar el Logout");	
+	}
+}
+
+function getType(i){
+	if (i==1)
+		return 'Noticia';
+	if (i==2)
+		return 'Evento';
+	if (i==3)
+		return 'Proyecto';
+}
+
+function getDate(){
+	var date = moment.unix(moment().utc().valueOf()/1000).format("MM/DD/YYYY");
+	console.log(date);
+}
+
+
+//reads data from the url
+function getUrlParameter(sParam) {
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+		sURLVariables = sPageURL.split('&'),
+		sParameterName,
+		i;
+
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=');
+		if (sParameterName[0] === sParam) 
+		{
+			return sParameterName[1] === undefined ? true : sParameterName[1];
+		}
 	}
 }
